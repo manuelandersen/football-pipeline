19881a6740225c6aa53e9b8670f101dda6fe34ea
manuelandersen <manuelandersen9@gmail.com>
2024-07-08 18:44:18 -0400

    feat: add initial mage files

---
+++ football-pipeline/data_loaders/gallant_oak.py
@@ -0,0 +1,49 @@
+import io
+import pandas as pd
+import requests
+from pandas import DataFrame
+import kaggle
+import os
+import subprocess
+import zipfile
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(**kwargs) -> DataFrame:
+    BASE_DIR = "data/"
+    DATASET_NAME = "player-scores"
+    DATASET_FILE = "club_games.csv"
+    dataset_path = os.path.join(BASE_DIR, DATASET_FILE + ".zip")
+
+    try:
+        result = subprocess.run(["kaggle", "datasets", "download", f"davidcariboo/{DATASET_NAME}", "-f", "club_games.csv", "-p", BASE_DIR])
+        if result.returncode == 0:
+            print("Dataset downloaded successfully.")
+            #print(result.stdout)
+            # Extract the dataset
+            with zipfile.ZipFile(dataset_path, "r") as zip_ref:
+                zip_ref.extractall(BASE_DIR)
+                print("Dataset extracted to:", BASE_DIR)
+                df = pd.read_csv(dataset_path)
+                #print(df.head(5))
+                return df
+        else:
+            print("Error downloading dataset:")
+            print(result.stderr)
+
+    except Exception as e:
+        print("Exception occurred while downloading dataset:")
+        print(str(e))
+
+
+@test
+def test_output(df) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert df is not None, 'The output is undefined'
