309131e6b76a87ba59a3ca4e9ed00d4500767d49
manuelandersen <manuelandersen9@gmail.com>
2024-07-10 18:16:42 -0400

    chore: add bigquery dataset

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,8 @@ resource "google_storage_bucket" "auto-expire" {
     }
   }
 }
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,25 +3,25 @@ variable "credentials" {
   default     = "~/.gc/credentials.json"
 }
 
-# variable "project" {
-#   description = "Project"
-#   default     = "football-pipeline-428816"
-# }
+variable "project" {
+  description = "Project"
+  default     = "football-pipeline-428816"
+}
 
-# variable "region" {
-#   description = "Region location"
-#   default     = "us-central1"
-# }
+variable "region" {
+  description = "Region location"
+  default     = "us-central1"
+}
 
 variable "location" {
   description = "Project Location"
   default     = "US"
 }
 
-# # variable "bq_dataset_name" {
-# #   description = "My BigQuery Dataset Name"
-# #   default     = "demo_dataset"
-# # }
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "raw_football_dataset"
+}
 
 # variable "cgs_bucket_name" {
 #   description = "My Storage Bucket Name"
